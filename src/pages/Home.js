fb7c6ed39438bfd9e2a9977c174b6341c9ba3a9c
brenolimeira <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>
2025-12-09 13:51:36 -0300

    adicionada ligacao com a api do backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,33 @@ import { Card, Flex, Typography } from 'antd';
 
 import rooms from '../mock/quartos.json'
 import { NavLink } from 'react-router-dom';
+import axios from "axios";
+import { useState, useEffect } from 'react';
 
 function Home() {
+
+    const [roomz, setRooms] = useState();
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/rooms/")
+            .then(response => setRooms(response.data));
+    }, []);
+
+    console.log(roomz)
 
     return (
         <Flex gap={16}>
             {rooms.map((room, index) => {
-                return(    
-                    <NavLink 
-                        key={index} 
+                return (
+                    <NavLink
+                        key={index}
                         to={`/room/${room.numero}`}
                         style={{ textDecoration: "none" }}
                     >
-                        <Card key={index} 
-                            title={<Typography.Title 
-                            level={3}>Quarto {room.numero}</Typography.Title>} 
-                            variant="borderless" 
+                        <Card key={index}
+                            title={<Typography.Title
+                                level={3}>Quarto {room.numero}</Typography.Title>}
+                            variant="borderless"
                             style={{ width: 300, backgroundColor: room.ocupado ? "#A30000" : '#0A7500' }}
                         >
                             <p>Hospedes : {room.quantidade_pessoas}</p>
---
+++ src/service.js
@@ -0,0 +1,6 @@
+// import axios from "axios";
+
+// useEffect(() => {
+//   axios.get("http://127.0.0.1:8000/api/rooms/")
+//     .then(response => setRooms(response.data));
+// }, []);
